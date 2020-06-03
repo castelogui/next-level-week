5cabd629e23ccb04f786ee64eb43d38f684202c6
castelogui <guilhermecastelo.mail@gmail.com>
2020-06-02 22:06:48 -0400

    add web aplication

---
+++ web/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        
+        <h1>Next Level Week</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
