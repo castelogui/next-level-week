8159259f9f4d72c87a46adcb9acee5a5d2dc11cb
castelogui <guilhermecastelo.mail@gmail.com>
2020-06-07 13:38:02 -0400

    update dependences

--- server/src/config/multer.ts
+++ server/src/config/multer.ts
@@ -4,7 +4,7 @@ import crypto from 'crypto';
 
 export default {
   storage: multer.diskStorage({
-    destination: path.resolve(__dirname, '..', '..', 'uploads'),
+    destination: path.resolve(__dirname, '..', '..', 'uploads', 'market'),
     filename(request, file, callback){
       const hash = crypto.randomBytes(6).toString('hex');
 
